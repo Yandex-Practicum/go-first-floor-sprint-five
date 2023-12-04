99b761b365c746ead99976fef7234d12d5fc3566
Ainur Muratov <141512722+Aiinnur@users.noreply.github.com>
2023-12-04 23:14:58 +0300

    Add check for duration == 0 in functions meanSpeed

--- main.go
+++ main.go
@@ -34,7 +34,11 @@ func (t Training) distance() float64 {
 // meanSpeed возвращает среднюю скорость бега или ходьбы.
 func (t Training) meanSpeed() float64 {
 	// вставьте ваш код ниже
-	return t.distance() / t.Duration.Hours()
+	if t.Duration == 0 {
+		return 0
+	}
+	distances := t.distance()
+	return distances / t.Duration.Hours()
 }
 
 // Calories возвращает количество потраченных килокалорий на тренировке.
