c50130f4e127e8ea19d6b5c618d2d2a6487e0522
Лукьянов Дмитрий <diman2421@mail.ru>
2024-01-01 14:25:23 +0300

    <Исправил func (w Walking) Calories() float64>

--- main.go
+++ main.go
@@ -132,8 +132,11 @@ type Walking struct {
 // * 0.029 * вес_спортсмена_в_кг) * время_тренировки_в_часах * мин_в_ч)
 // Это переопределенный метод Calories() из Training.
 func (w Walking) Calories() float64 {
-	meanSpeed := w.meanSpeed() * KmHInMsec
-	return (CaloriesWeightMultiplier*w.Weight + (math.Pow(meanSpeed, 2)/(w.Height/CmInM))*CaloriesSpeedHeightMultiplier*w.Weight) * w.Duration.Hours() * MinInHours
+
+	heughtInMetrs := w.Height / CmInM // перевожу высоту спортсмена в метры
+	speedHeight := w.meanSpeed() * KmHInMsec
+
+	return (CaloriesWeightMultiplier*w.Weight + ((math.Pow(speedHeight, 2) / heughtInMetrs) * CaloriesSpeedHeightMultiplier * w.Weight)) * w.Duration.Hours() * MinInHours
 }
 
 // TrainingInfo возвращает структуру InfoMessage с информацией о проведенной тренировке.
